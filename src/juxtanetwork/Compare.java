cae69222305eabf3f5faad773d9175a84baf2ff8
ankouros30@gmail.com <koag@DA140340004248A.intracomtel.com>
2018-10-12 15:18:42 +0300

    New class Compare

--- src/juxtanetwork/Compare.java
+++ src/juxtanetwork/Compare.java
@@ -3,16 +3,22 @@
 package juxtanetwork;
 
 import java.util.ArrayList;
+import java.util.Properties;
 import javax.swing.JList;
 import javax.swing.tree.DefaultMutableTreeNode;
 
 public class Compare {
 
+    String fileSeperator = System.getProperty("path.separator");
+    private String LogsDirectory = "Data";
+    private String timeStampBase = "2018-10-11_16_42_17";  // will be retrieved later
+    private String timeStampTarget = "2018-10-11_16_42_18";  // will be retrieved later
+
     private ArrayList<String> selectedCommands = new ArrayList<String>();
     private ArrayList<String> BaseNodes = new ArrayList<String>();
     private ArrayList<String> TargetNodes = new ArrayList<String>();
 
-    
+    //Base Files Path's
     // Empty yet Constructor
     public Compare() {
     }
@@ -29,19 +35,54 @@ public class Compare {
 
     //Update BaseNodes ArrayList
     void updateBaseNodes(JList<String> compList1) {
-        BaseNodes.clear();      
+        BaseNodes.clear();
         for (int i = 0; i < compList1.getModel().getSize(); i++) {
             BaseNodes.add(compList1.getModel().getElementAt(i));
         }
         System.out.println("Selected Base Nodes : " + BaseNodes);
     }
-    
+
     //Update TargetNodes ArrayList
     void updateTargetNodes(JList<String> compList2) {
-        TargetNodes.clear();      
+        TargetNodes.clear();
         for (int i = 0; i < compList2.getModel().getSize(); i++) {
             TargetNodes.add(compList2.getModel().getElementAt(i));
         }
         System.out.println("Selected Target Nodes : " + TargetNodes);
     }
+
+    //Update timeStampBase String
+    void updateTimeStampBase(JList<String> compList2) {
+        // To be implemented when timestamp selection implemented
+
+    }
+
+    //Update timeStampBase String
+    void updateTimeStampTarget(JList<String> compList2) {
+        // To be implemented when timestamp selection implemented
+    }
+
+    void getPOintoHash() {
+        //======Check if all data exists, do something.. e.g. popup
+        if (selectedCommands.size() == 0) {
+            return;
+        }
+        if (BaseNodes.size() == 0) {
+            return;
+        }
+        if (TargetNodes.size() == 0) {
+            return;
+        }
+        if (timeStampBase.equals("")) {
+            return;
+        }
+        if (timeStampTarget.equals("")) {
+            return;
+        }
+//=============================================
+
+
+
+    }
+
 }
